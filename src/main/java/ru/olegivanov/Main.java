194252b26a94277284fb42387fc291c4f90ca5db
Black Smith-JD-40 <ol.channel.t@gmail.com>
2023-03-19 23:18:37 +0400

    Initial commit

---
+++ src/main/java/ru/olegivanov/Main.java
@@ -0,0 +1,38 @@
+package ru.olegivanov;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        final  String url = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+        ObjectMapper mapper = new ObjectMapper();
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)
+                        .setSocketTimeout(30000)
+                        .setRedirectsEnabled(false)
+                        .build())
+                .build();
+        HttpGet request = new HttpGet(url);
+        request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+        CloseableHttpResponse response = httpClient.execute(request);
+        //Arrays.stream(response.getAllHeaders()).forEach(System.out::println);
+        List<CatsPost> catsPost = mapper.readValue(
+                response.getEntity().getContent(), new
+                        TypeReference<>(){});
+        catsPost.forEach(System.out::println);
+
+    }
+}
